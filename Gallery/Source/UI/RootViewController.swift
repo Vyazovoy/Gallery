9b0d5b99228523d88d6bc608e41915ca26e8418c
Andrew Vyazovoy <andrew.vyazovoy@gmail.com>
2014-11-28 01:29:12 +0300

    Base project added.

---
+++ Gallery/Source/UI/RootViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RootViewController.swift
+//  Gallery
+//
+//  Created by Andrew Vyazovoy on 28.11.14.
+//  Copyright (c) 2014 My Corp. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
