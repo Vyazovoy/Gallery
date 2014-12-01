ff56e0c5ff5200af424088d70992df0b43e12159
Andrew Vyazovoy <andrew.vyazovoy@gmail.com>
2014-12-01 04:45:04 +0300

    Custom SDWebImage added.

---
+++ Gallery/Gallery-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Gallery-Bridging-Header.h
+//  Gallery
+//
+//  Created by Andrew Vyazovoy on 30.11.14.
+//  Copyright (c) 2014 My Corp. All rights reserved.
+//
+
+#ifndef Gallery_Gallery_Bridging_Header_h
+#define Gallery_Gallery_Bridging_Header_h
+
+#import <SDWebImage/UIImageView+WebCache.h>
+
+#endif
